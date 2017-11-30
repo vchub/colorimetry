baae43405bd5a56d20a3f81c737a7cc24a1d6e3c
vlad chub <vlad.chub@gmail.com>
2017-12-01 04:17:03 +0600

    index.html

--- matrix.js
+++ matrix.js
@@ -33,7 +33,6 @@ const Lab_to_XYZ = (l, a, b, d_white) => {
   let X = Xn * f_1((l + 16) / 116 + a / 500)
   let Y = Yn * f_1((l + 16) / 116)
   let Z = Zn * f_1((l + 16) / 116 - b / 200)
-    // cl('X,Y,Z: ', X, Y, Z)
   return [X, Y, Z]
 
 }
@@ -84,7 +83,7 @@ const Lab_to_SRGB = (l, a, b, d_white) => {
 const Lab_to_Gost = (l, a, b, d_white = 65) => {
   let lab = [l,a,b,d_white].map(x=> x*1.0)
   let rgb = Lab_to_SRGB.apply(null, lab)
-  cl('rgb', rgb)
+  // cl('rgb', rgb)
   let g = rgb[1] / asum(rgb)
   let c = round_to_half(10.7460 - 25.39 * g)
   return [rgb, c]
@@ -134,14 +133,10 @@ const formHandler = () => {
     const b = form.elements.b.value
     const d_white = form.elements.dwhite.value || 65
 
-    cl(l, a, b, d_white)
-
     const got = Lab_to_Gost(l, a, b, d_white)
     const rgb = got[0]
     const gost = got[1]
-    cl(got)
-    cl(rgb)
-    cl(gost)
+
     const thergb = `rgb(${rgb[0]},${rgb[1]},${rgb[2]})`
     circle.style.background = thergb
     res.innerHTML = gost
